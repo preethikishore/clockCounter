973c3482bc79523e39cc4b164915808bd9c8e3bb
preethikishore <preethikishre@gmail.com>
2021-01-14 13:45:50 +0000

    final commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 let clockH = document.querySelector('.clock_hours');
 let clockM = document.querySelector('.clock_minutes');
 let clockS = document.querySelector('.clock_seconds');
-let finalDate = new Date('Jul 25,2020 00:00:00');
-let seconds = 1000;
-let minutes = seconds * 60;
-let hour = minutes * 60;
+let countdownDays = document.querySelector('.countdown_days');
+let countdownHours = document.querySelector('.countdown_hours');
+let countdownMinutes = document.querySelector('.countdown_minutes');
+let countdownSeconds = document.querySelector('.countdown_seconds');
+let finalDate = new Date('Jan 01,2021 00:30:00');
+let second = 1000;
+let minute = second * 60;
+let hour = minute * 60;
 let day = hour * 24;
+
 
 let upDateTime = () => {
     let now = new Date();
@@ -19,7 +24,32 @@ let upDateTime = () => {
 let startclock = () =>
 {
     upDateTime ();
-  setInterval(upDateTime,1000);
+    setInterval( () =>
+    {
+      upDateTime();
+      updateCountDown();
+      
+     } , 1000);
 }
 
+
+let updateCountDown = () => {
+  let now = new Date();
+  let diff = now -  finalDate ;
+  let diffObject = converMilliDHMS(diff);
+  countdownDays.textContent = diffObject.days >=10 ? diffObject.days :'0'+diffObject.days;
+  countdownMinutes.textContent = diffObject.minutes >=10 ? diffObject.minutes :'0'+diffObject.minutes;
+  countdownHours.textContent = diffObject.hours >=10 ? diffObject.hours :'0'+diffObject.hours;
+  countdownSeconds.textContent = diffObject.seconds >=10 ? diffObject.seconds :'0'+diffObject.seconds;
+}
+
+let converMilliDHMS = (millis) =>
+{
+  let days = Math.floor(millis / day);
+  let hours = Math.floor(millis % day / hour);
+  let minutes = Math.floor(millis % hour / minute);
+  let seconds = Math.floor(millis % minute / second);
+  return{days,hours,minutes,seconds};
+
+}
 startclock();
